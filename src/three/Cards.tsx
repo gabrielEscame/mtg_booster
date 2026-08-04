66ad2d56413436796f3e1463b81ab1908efdcdf8
Gabriel Escame <gabriel.escame@gmail.com>
2026-08-04 01:06:02 +0100

    refactor: add separate cardsIdleRef for idle animation handling in Cards component

--- src/three/Cards.tsx
+++ src/three/Cards.tsx
@@ -93,27 +93,30 @@ const Cards = () => {
   const { scene: mythicCard } = useGLTF('/models/mythic_card.glb')
   const { scene: rareCard } = useGLTF('/models/rare_card.glb')
   const cardsMouseRef = useRef<THREE.Group | null>(null)
+  const cardsIdleRef = useRef<THREE.Group | null>(null)
   const mythicCardRef = useRef<THREE.Group | null>(null)
   const rareCardRef = useRef<THREE.Group | null>(null)
 
   useFrame(({ pointer, clock }) => {
-    if (!cardsMouseRef.current) return
+    if (!cardsMouseRef.current || !cardsIdleRef) return
 
     updateIdleAnimaation({ ref: cardsMouseRef, clock })
-    updateMouseAnimation({ ref: cardsMouseRef, pointer })
+    updateMouseAnimation({ ref: cardsIdleRef, pointer })
   })
 
   return (
     <group ref={cardsMouseRef} position={[0.08, 0, 0]}>
-      <group ref={mythicCardRef}>
-        <primitive object={mythicCard} scale={1} />
-      </group>
+      <group ref={cardsIdleRef}>
+        <group ref={mythicCardRef}>
+          <primitive object={mythicCard} scale={1} />
+        </group>
 
-      <group ref={rareCardRef}>
-        <primitive object={rareCard} scale={1} position={[0, 0, -0.0005]} />
-      </group>
+        <group ref={rareCardRef}>
+          <primitive object={rareCard} scale={1} position={[0, 0, -0.0005]} />
+        </group>
 
-      <CardsStack />
+        <CardsStack />
+      </group>
     </group>
   )
 }
